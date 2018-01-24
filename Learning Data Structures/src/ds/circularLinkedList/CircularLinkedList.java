6b08facdddd9f662d191e62ed71753807533daf1
Harsh Patel <harshpatel7.list@gmail.com>
2018-01-24 10:23:51 -0500

    Learned about Circular Linked List
    Began Making the Classes and Constructors

--- Learning Data Structures/src/ds/circularLinkedList/CircularLinkedList.java
+++ Learning Data Structures/src/ds/circularLinkedList/CircularLinkedList.java
@@ -2,4 +2,27 @@ package ds.circularLinkedList;
 
 public class CircularLinkedList {
 
+	private Node first; 
+	private Node last; 
+	
+	public CircularLinkedList() {
+		Node first = null;
+		Node last = null; 
+	}
+	
+	public void insertFirst(int data) {
+		Node newNode = new Node(); 
+		newNode.data = data; 
+		
+		if(isEmpty()) {
+			last = newNode; 
+		}
+		newNode.next = first; 
+		first  = newNode; 
+	}
+	
+	private boolean isEmpty() {
+		return first == null; 
+	}
+	
 }
